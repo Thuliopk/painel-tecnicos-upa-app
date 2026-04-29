c9c995f21668d10e054ff6f8cb29f934d2c077fb
Thuliopk <90077419+Thuliopk@users.noreply.github.com>
2026-04-29 17:37:44 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Só intercepta.
+  // Sem cache por enquanto, para evitar travar versão antiga do painel.
+});
